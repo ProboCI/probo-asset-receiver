de5b6293b9ea20a0f2e51238e4476b755bee54f2
allierays <alliejones42@gmail.com>
2016-01-06 12:21:45 -0500

    Adding AwsS3 test to test s3 upload.

---
+++ test/awsS3-test.js
@@ -0,0 +1,77 @@
+'use strict';
+var should = require('should');
+var through2 = require('through2');
+var split2 = require('split2');
+var path = require('path');
+var request = require('request');
+var memdown = require('memdown');
+var bunyan = require('bunyan');
+var fs = require('fs');
+var AWSMock = require('mock-aws-s3');
+var mock = require('mock-require');
+mock('aws-sdk', 'mock-aws-s3');
+
+var app = require('..');
+var AwsS3Storage = app.lib.plugins.fileStorage.AwsS3Storage;
+
+describe('AWS file sotrage plugin', function () {
+  describe('create write stream', function () {
+    it('should successfully store and retrieve a file', function (done) {
+
+        var awsOptions = {
+          awsAccessKeyId: 'anyAccessKeyId',
+          awsSecretAccessKey: 'anySecretAccessKey',
+          awsBucket: 'anyBucket',
+        };
+        var logger = bunyan.createLogger({
+          name: 'tests',
+          level: 'fatal',
+          stream: fs.createWriteStream('/dev/null'),
+        });
+        var awsStore = new AwsS3Storage(awsOptions, logger);
+        awsStore.s3.upload = function (search, options, callback) {
+          if (typeof options === 'function' && callback === undefined) {
+            callback = options;
+            options = null;
+          }
+          var self = this;
+          return {
+            send: function (cb) {
+              if (!cb && callback) {
+                cb = callback;
+              }
+              return self.putObject(search, cb);
+            }
+          };
+        };
+        var assetId = 'testUpload-' + Date.now() + '.txt';
+
+        var uploadComplete = function (error) {
+          var dataRead = [];
+          var readStream = awsStore.createReadStream(assetId);
+
+          var reader = function (data, enc, callback) {
+            dataRead.push(data.toString('utf8'));
+            callback(null, data);
+          };
+          var checker = function () {
+            dataRead.length.should.equal(2);
+            dataRead[0].should.equal('One line')
+            dataRead[1].should.equal('Two lines')
+            done();
+          };
+          readStream
+            .pipe(split2())
+            .pipe(through2(reader, checker))
+        };
+
+        var writeStream = awsStore.createWriteStream(assetId, uploadComplete);
+
+        writeStream.write('One line\n');
+        writeStream.write('Two lines\n');
+        writeStream.end();
+      }
+    )
+    ;
+  });
+});
